f2e97c4e776ee92358c8e14935a6e9af9b45524b
timefliez1210 <crfabig@gmail.com>
2020-03-12 16:34:33 +0800

    Updates

---
+++ precache-manifest.50e803518f61a3df8acdc8922c725a70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd5a3491bd70a8b290e801b9eb230248",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "367b1b297460e50fecdd",
+    "url": "/monsters-rolodex/static/css/main.49d01a13.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "367b1b297460e50fecdd",
+    "url": "/monsters-rolodex/static/js/main.ff5d40f3.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
